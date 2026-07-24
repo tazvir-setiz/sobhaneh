723d7c13c8929adf66f53d51d4dad70e33fb8f9f
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-24 18:57:27 +0330

    edit USER DTO

---
+++ src/main/java/ir/sobhan/sobhaneh/common/dto/UserDTO.java
@@ -0,0 +1,52 @@
+//in the name of ALLAH
+//YA MAHDI
+package ir.sobhan.sobhaneh.common.dto;
+
+public class UserDTO {
+
+    private int id;
+    private String phoneNumber;
+    private String password;
+    private String username;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(int id, String phoneNumber, String password) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
