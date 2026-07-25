e3bebaacb79b27a5079d11ba3e4030c99a00c364
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-25 09:31:08 +0330

    edit hostdto

--- src/main/java/ir/sobhan/sobhaneh/common/dto/HostDTO.java
+++ src/main/java/ir/sobhan/sobhaneh/common/dto/HostDTO.java
@@ -1,37 +1,54 @@
 package ir.sobhan.sobhaneh.common.dto;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HostDTO {
     private String ip;
     private int startPort;
     private int endPort;
+    private final HashSet<Integer> unusedPorts = new HashSet<>();
 
     public HostDTO(String ip, int startPort, int endPort) {
         this.ip = ip;
         this.startPort = startPort;
         this.endPort = endPort;
+        for (int i = startPort; i <= endPort; i++) {
+            unusedPorts.add(i);
+        }
     }
 
     public String getIp() {
         return ip;
     }
 
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
     public int getStartPort() {
         return startPort;
-    }
-
-    public void setStartPort(int startPort) {
-        this.startPort = startPort;
     }
 
     public int getEndPort() {
         return endPort;
     }
 
-    public void setEndPort(int endPort) {
-        this.endPort = endPort;
+    public int getFreePort() {
+        if(unusedPorts.isEmpty())
+            return -1;
+        Iterator<Integer> iterator = unusedPorts.iterator();
+        return iterator.next().intValue();
+    }
+    public boolean occupyPort(int port) {
+        if (unusedPorts.contains(port)) {
+            unusedPorts.remove(port);
+            return true;
+        }
+        return false;
+    }
+    public boolean releasePort(int port) {
+        if(port >= startPort && port <= endPort) return false;
+        if (unusedPorts.contains(port)) {
+            return false;
+        }
+        unusedPorts.add(port);
+        return true;
     }
 }
