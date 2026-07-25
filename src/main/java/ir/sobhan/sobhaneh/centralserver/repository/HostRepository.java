35c90f415af5114355c70464a41c77005890e207
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-25 23:01:23 +0330

    edit getFree host add random

--- src/main/java/ir/sobhan/sobhaneh/centralserver/repository/HostRepository.java
+++ src/main/java/ir/sobhan/sobhaneh/centralserver/repository/HostRepository.java
@@ -7,26 +7,28 @@ import ir.sobhan.sobhaneh.common.dto.HostDTO;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class HostRepository {
     private static final HashMap<String, ArrayList<HostDTO>> hosts = new HashMap<>();
-    private HostRepository() {}
+
+    private HostRepository() {
+    }
 
     private static boolean portConflict(int oStart, int oEnd, int nStart, int nEnd) {
-        if((nStart <= oEnd) && (nEnd >= oStart)) return true;
-        return false;
+        return (nStart <= oEnd) && (nEnd >= oStart);
     }
 
     public static boolean addHost(HostDTO host) {
         if (host == null) {
             return false;
         }
-        if(!hosts.containsKey(host.getIp())){
+        if (!hosts.containsKey(host.getIp())) {
             hosts.put(host.getIp(), new ArrayList<>());
         }
         ArrayList<HostDTO> list = hosts.get(host.getIp());
         for (HostDTO oldHost : list) {
-            if(portConflict(oldHost.getStartPort(), oldHost.getEndPort(), host.getStartPort(), host.getEndPort())) {
+            if (portConflict(oldHost.getStartPort(), oldHost.getEndPort(), host.getStartPort(), host.getEndPort())) {
                 return false;
             }
         }
@@ -43,7 +45,7 @@ public class HostRepository {
             HostDTO oldHost = list.get(i);
             if (oldHost.getStartPort() == startPort && oldHost.getEndPort() == endPort) {
                 list.remove(oldHost);
-                if(list.isEmpty()) {
+                if (list.isEmpty()) {
                     hosts.remove(ip);
                 }
                 return true;
@@ -51,8 +53,9 @@ public class HostRepository {
         }
         return false;
     }
+
     public static HostDTO findHost(String ip, int startPort, int endPort) {
-        if(hosts.get(ip) == null) {
+        if (hosts.get(ip) == null) {
             return null;
         }
         ArrayList<HostDTO> list = hosts.get(ip);
@@ -64,14 +67,20 @@ public class HostRepository {
         return null;
     }
 
-    public static HostDTO getFreeHost(){
-        for(String ip : hosts.keySet()){
-            ArrayList<HostDTO> hostsList = hosts.get(ip);
-            for(HostDTO host : hostsList){
-                if(host.getFreePort() != -1) return host;
+    public static HostDTO getFreeHost() {
+        ArrayList<HostDTO> freeHosts = new ArrayList<>();
+        for (ArrayList<HostDTO> list : hosts.values()) {
+            for (HostDTO host : list) {
+                if (host.getFreePort() != -1) {
+                    freeHosts.add(host);
+                }
             }
         }
-        return null;
+        if (freeHosts.isEmpty()) {
+            return null;
+        }
+        Random random = new Random();
+        return freeHosts.get(random.nextInt(freeHosts.size()));
     }
 
     public static ArrayList<HostDTO> getHosts(String ip) {
