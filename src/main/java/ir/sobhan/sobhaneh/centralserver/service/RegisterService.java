f6520e001649e4707d046fe2ac9a3f44a43d4df0
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-25 10:52:34 +0330

    move check func into Checkers

--- src/main/java/ir/sobhan/sobhaneh/centralserver/service/RegisterService.java
+++ src/main/java/ir/sobhan/sobhaneh/centralserver/service/RegisterService.java
@@ -8,27 +8,6 @@ import ir.sobhan.sobhaneh.common.response.ResponseStatus;
 
 public class RegisterService {
 
-    private Response checkPhoneNumber(String phN) {
-        if (phN == null) return new Response(ErrorType.PHONE_NUMBER_INVALID);
-        if (phN.length() != 11) return new Response(ErrorType.PHONE_NUMBER_INVALID);
-        if (phN.startsWith("09") == false) return new Response(ErrorType.PHONE_NUMBER_INVALID);
-        for (int i = 0; i < phN.length(); i++) {
-            if (!Character.isDigit(phN.charAt(i))) {
-                return new Response(ErrorType.PHONE_NUMBER_INVALID);
-            }
-        }
-
-        return new Response(ResponseStatus.OK);
-    }
-
-    private Response checkPassword(String password) {
-        if (password == null) return new Response(ErrorType.PASSWORD_INVALID);
-        if (password.isBlank()) return new Response(ErrorType.PASSWORD_INVALID);
-        if (password.length() < 6) return new Response(ErrorType.PASSWORD_INVALID);
-
-        return new Response(ResponseStatus.OK);
-    }
-
     public RegisterService() {
     }
 
---
+++ src/main/java/ir/sobhan/sobhaneh/common/Checkers.java
@@ -0,0 +1,29 @@
+package ir.sobhan.sobhaneh.common;
+
+import ir.sobhan.sobhaneh.common.response.ErrorType;
+import ir.sobhan.sobhaneh.common.response.Response;
+import ir.sobhan.sobhaneh.common.response.ResponseStatus;
+
+public class Checkers {
+    private Checkers() {}
+    private static Response checkPhoneNumber(String phN) {
+        if (phN == null) return new Response(ErrorType.PHONE_NUMBER_INVALID);
+        if (phN.length() != 11) return new Response(ErrorType.PHONE_NUMBER_INVALID);
+        if (phN.startsWith("09") == false) return new Response(ErrorType.PHONE_NUMBER_INVALID);
+        for (int i = 0; i < phN.length(); i++) {
+            if (!Character.isDigit(phN.charAt(i))) {
+                return new Response(ErrorType.PHONE_NUMBER_INVALID);
+            }
+        }
+
+        return new Response(ResponseStatus.OK);
+    }
+
+    private static Response checkPassword(String password) {
+        if (password == null) return new Response(ErrorType.PASSWORD_INVALID);
+        if (password.isBlank()) return new Response(ErrorType.PASSWORD_INVALID);
+        if (password.length() < 6) return new Response(ErrorType.PASSWORD_INVALID);
+
+        return new Response(ResponseStatus.OK);
+    }
+}
