a8d5c33a1e5d4a51468c77a6239235fbe90c6ae5
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-29 09:38:55 +0330

    add CentralServer

--- src/main/java/ir/sobhaneh/central/CentralServer.java
+++ src/main/java/ir/sobhaneh/central/CentralServer.java
@@ -2,6 +2,20 @@ package ir.sobhaneh.central;
 
 import ir.sobhaneh.central.models.Connection;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class CentralServer {
+    public static void main() {
+        try (ServerSocket serverSocket = new ServerSocket(8000)){
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket)).start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 }
