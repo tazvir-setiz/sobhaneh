780d9de1bcab55bb1ab1bfa63b0e51ce29449475
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-08-01 15:00:23 +0330

    change into record

--- src/main/java/ir/sobhaneh/central/models/WorkspaceInfo.java
+++ src/main/java/ir/sobhaneh/central/models/WorkspaceInfo.java
@@ -3,32 +3,5 @@
 
 package ir.sobhaneh.central.models;
 
-public class WorkspaceInfo {
-    private final String name;
-    private final String hostIp;
-    private final int port;
-    private final long creatorUserId;
-
-    public WorkspaceInfo(String name, String hostIp, int port, long creatorUserId) {
-        this.name = name;
-        this.hostIp = hostIp;
-        this.port = port;
-        this.creatorUserId = creatorUserId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getHostIp() {
-        return hostIp;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public long getCreatorUserId() {
-        return creatorUserId;
-    }
+public record WorkspaceInfo(String name, String hostIp, int port, long creatorUserId) {
 }
--- src/main/java/ir/sobhaneh/host/HostConfig.java
+++ src/main/java/ir/sobhaneh/host/HostConfig.java
@@ -3,26 +3,4 @@
 
 package ir.sobhaneh.host;
 
-public class HostConfig {
-    private final String ip;
-    private final int startPort;
-    private final int endPort;
-
-    public HostConfig(String ip, int startPort, int endPort) {
-        this.ip = ip;
-        this.startPort = startPort;
-        this.endPort = endPort;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public int getStartPort() {
-        return startPort;
-    }
-
-    public int getEndPort() {
-        return endPort;
-    }
-}
+public record HostConfig(String  hostIp, int startPort, int endPort) {}
