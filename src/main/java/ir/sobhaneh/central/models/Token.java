86da8bba0ea883c5841690ec6580212e9ef71f8a
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-08-01 15:02:42 +0330

    add expired method

--- src/main/java/ir/sobhaneh/central/models/Token.java
+++ src/main/java/ir/sobhaneh/central/models/Token.java
@@ -1,4 +1,7 @@
 package ir.sobhaneh.central.models;
 
 public record Token(String token, long creatorUserId, String workspaceName, long expiresAtMillis) {
+    public boolean isExpired() {
+        return expiresAtMillis < System.currentTimeMillis();
+    }
 }
