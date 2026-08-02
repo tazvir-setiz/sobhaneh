5dee2fe3f355acfc2aee5ce0f8469231ceeeec0c
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-08-02 14:33:09 +0330

    add chat

---
+++ src/main/java/ir/sobhaneh/central/models/Chat.java
@@ -0,0 +1,12 @@
+package ir.sobhaneh.central.models;
+
+public class Chat {
+    String usernameA;
+    String usernameB;
+    int lastSeq;
+
+    public static String buildKey(String usernameA, String usernameB) {
+        if(usernameA.compareTo(usernameB) >  0) return usernameA + "-"  + usernameB;
+        else return usernameA + "-" + usernameB;
+    }
+}
