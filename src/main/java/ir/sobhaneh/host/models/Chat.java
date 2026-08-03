a3ee2fd201f7780a82f977cdd339841e820aba0d
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-08-03 10:20:53 +0330

    add unread

--- src/main/java/ir/sobhaneh/host/models/Chat.java
+++ src/main/java/ir/sobhaneh/host/models/Chat.java
@@ -1,3 +1,6 @@
+//in the name of ALLAH
+//YA MAHDI
+
 package ir.sobhaneh.host.models;
 
 import lombok.Getter;
@@ -14,6 +17,8 @@ public class Chat {
     private final String usernameB;
     private final List<Message> messages = new CopyOnWriteArrayList<>();
     private final AtomicInteger lastSeq = new AtomicInteger(0);
+    private final AtomicInteger unreadCountForA = new AtomicInteger(0);
+    private final AtomicInteger unreadCountForB = new AtomicInteger(0);
 
     public static String buildKey(String usernameA, String usernameB) {
         if (usernameA.compareTo(usernameB) > 0) {
@@ -29,5 +34,33 @@ public class Chat {
 
     public void addMessage(Message message) {
         messages.add(message);
+    }
+
+    public void incrementUnreadFor(String username) {
+        if (isUserA(username)) {
+            unreadCountForA.incrementAndGet();
+        } else {
+            unreadCountForB.incrementAndGet();
+        }
+    }
+
+    public int getUnreadCountFor(String username) {
+        if (isUserA(username)) {
+            return unreadCountForA.get();
+        } else {
+            return unreadCountForB.get();
+        }
+    }
+
+    public void clearUnreadFor(String username) {
+        if (isUserA(username)) {
+            unreadCountForA.set(0);
+        } else {
+            unreadCountForB.set(0);
+        }
+    }
+
+    private boolean isUserA(String username) {
+        return usernameA.equals(username);
     }
 }
